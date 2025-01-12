8e3ea27c9e9affb419104ee772415c28430fbc7d
Todd <toddcford@gmail.com>
2025-01-12 15:40:37 -0600

    routing and store setup

---
+++ GUI/webapp/src/app/store.ts
@@ -0,0 +1,33 @@
+import type { Action, ThunkAction } from "@reduxjs/toolkit"
+import { combineSlices, configureStore } from "@reduxjs/toolkit"
+import { setupListeners } from "@reduxjs/toolkit/query"
+import { apiSlice } from "@/api/apiSlice"
+
+const rootReducer = combineSlices(apiSlice)
+
+export type RootState = ReturnType<typeof rootReducer>
+
+export const makeStore = (preloadedState?: Partial<RootState>) => {
+    const store = configureStore({
+        reducer: rootReducer,
+        middleware: getDefaultMiddleware => {
+            return getDefaultMiddleware().concat(apiSlice.middleware)
+        },
+        preloadedState,
+    })
+
+    setupListeners(store.dispatch)
+
+    return store
+}
+
+export const store = makeStore()
+
+export type AppStore = typeof store
+export type AppDispatch = AppStore["dispatch"]
+export type AppThunk<ThunkReturnType> = ThunkAction<
+    ThunkReturnType,
+    RootState,
+    unknown,
+    Action
+>
--- GUI/webapp/src/main.tsx
+++ GUI/webapp/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './store'
-import App from './App'
-import './styles/index.scss'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { Provider } from "react-redux"
+import { store } from "~/app/store"
+import { BrowserRouter } from "react-router-dom"
+import App from "./App"
+import "./styles/index.scss"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
 )
