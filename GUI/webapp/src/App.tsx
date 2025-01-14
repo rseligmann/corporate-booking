4b953008a23793e9aa091735d01b1043d94d3635
GetTheCamera <31111521+GetTheCamera@users.noreply.github.com>
2025-01-14 11:23:50 -0600

    Add App component with layout configuration

--- GUI/webapp/src/App.tsx
+++ GUI/webapp/src/App.tsx
@@ -1,33 +1,13 @@
-import { FC } from "react"
-import { Routes, Route } from "react-router-dom"
-import { DefaultLayout } from "@/layouts/DefaultLayout"
-import { AuthLayout } from "@/layouts/AuthLayout"
-import { Login } from "@/routes/login"
-import { RequireAuth } from "@/features/auth/RequireAuth"
-import "~/styles/App.scss"
+import React from 'react';
+import './styles/global.scss';
 
-const App: FC = () => {
-    return (
-        <Routes>
-            {/* Auth routes */}
-            <Route element={<AuthLayout />}>
-                <Route path="/login" element={<Login />} />
-                {/* Add other auth routes like register, forgot-password here */}
-            </Route>
+// Since we're not using Next.js, we'll use @font-face for fonts
+const App: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  return (
+    <div className="app-root antialiased">
+      {children}
+    </div>
+  );
+};
 
-            {/* Protected routes */}
-            <Route
-                path="/"
-                element={
-                    <RequireAuth>
-                        <DefaultLayout />
-                    </RequireAuth>
-                }
-            >
-                <></>
-            </Route>
-        </Routes>
-    )
-}
-
-export default App
+export default App;
